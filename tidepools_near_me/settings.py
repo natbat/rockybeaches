c1e3dab0ff7f5c1e313400b9e7baf5b80c6b8c9e
Natalie Downe <nat@natbat.net>
2017-11-25 18:28:05 -0800

    Configured whitenoise

--- tidepools_near_me/settings.py
+++ tidepools_near_me/settings.py
@@ -135,3 +135,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
--- tidepools_near_me/wsgi.py
+++ tidepools_near_me/wsgi.py
@@ -1,16 +1,8 @@
-"""
-WSGI config for tidepools_near_me project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/dev/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tidepools_near_me.settings")
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
