0cf6f1a84d948c91512fb2d01ac850995c34c55c
Simon Willison <swillison@gmail.com>
2020-08-17 15:16:35 -0700

    New Datasette prototype

--- home/admin.py
+++
@@ -1,4 +0,0 @@
-from .models import Station
-from django.contrib.admin import site
-
-site.register(Station)
--- home/views.py
+++
@@ -1,5 +0,0 @@
-from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
-from django.shortcuts import render
-
-from wagtail.core.models import Page
-from wagtail.search.models import Query
