ec102c6da5a5d86f17628740d90b6365b671b5e1
Simon Willison <swillison@gmail.com>
2020-08-18 15:08:36 -0700

    /us/pillar-point now serves page, closes #6
    I got this working with a diabolical internal redirect scheme based
    on rewriting the ASGI scope and then re-dispatching to datasette.app()

--- plugins/redirect_homepage.py
+++
@@ -1,7 +0,0 @@
-from datasette import hookimpl
-from datasette.utils.asgi import Response
-
-
-@hookimpl
-def register_routes():
-    return ((r"^/$", lambda: Response.redirect("/data/places/pillar-point")),)
