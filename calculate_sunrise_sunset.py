00c9d92548d7d099adfba829f555d7ef2fcd54b1
Simon Willison <swillison@gmail.com>
2020-08-18 13:13:45 -0700

    Calculate and store sunrise/sunset, refs #11

---
+++ calculate_sunrise_sunset.py
@@ -0,0 +1,45 @@
+from astral import LocationInfo, sun
+import datetime
+import httpx
+import pytz
+import sqlite_utils
+import sys
+from urllib.parse import urlencode
+
+
+def calculate_sunrise_sunset(place):
+    yesterday = datetime.date.today() - datetime.timedelta(days=1)
+    end_date = yesterday + datetime.timedelta(days=32)
+    location_info = LocationInfo(
+        place["address"], "", place["time_zone"], place["latitude"], place["longitude"]
+    )
+    day = yesterday
+    day_infos = []
+    tz = pytz.timezone(place["time_zone"])
+    while day <= end_date:
+        info = sun.sun(location_info.observer, date=day)
+        day_info = {
+            "place": place["slug"],
+            "day": day.isoformat(),
+        }
+        day_info.update(
+            {
+                key: value.astimezone(tz).time().isoformat(timespec="seconds")
+                for key, value in info.items()
+            }
+        )
+        day_infos.append(day_info)
+        day += datetime.timedelta(days=1)
+    return day_infos
+
+
+if __name__ == "__main__":
+    assert sys.argv[-1].endswith(".db")
+    db = sqlite_utils.Database(sys.argv[-1])
+    table = db.table(
+        "sunrise_sunset",
+        pk=("place", "day"),
+        foreign_keys=(("place", "places", "slug"),),
+    )
+    for place in db["places"].rows:
+        table.insert_all(calculate_sunrise_sunset(place), replace=True)
--- scripts/build.sh
+++ scripts/build.sh
@@ -3,3 +3,4 @@ rm -f data.db || true
 yaml-to-sqlite data.db stations data/stations.yml --pk=id
 yaml-to-sqlite data.db places data/places.yml --pk=slug
 python fetch_noaa_tide_times.py data.db
+python calculate_sunrise_sunset.py data.db
